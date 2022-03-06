d6ea224940f41e9e197a96ef53e9c518bab60150
Manas Mishra <manas.m22@gmail.com>
2022-03-06 16:24:54 +0530

    Clone completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,14 @@
-<main>
-	<h1 class="bg-slate-800 text-white px-8">HELLO</h1>	
+<script>
+	import LoginCard from './LoginCard.svelte' 
+</script>
+
+<main class="flex items-center justify-center bg-gray-100 h-screen">
+	<div class="w-1/3">
+		<img class="w-80" src="/images/fb.svg" alt="Facebook Logo" />
+		<p class="mx-8 font-sans text-2xl">Facebook helps you connect and share with the people in your life.</p>
+	</div>
+	<div class="w-1/4 m-6 flex flex-col">
+		<LoginCard />
+		<div class="text-sm mt-6 self-center"><span class="font-bold hover:underline cursor-pointer">Create a Page</span> for a celebrity, brand or business.</div>
+	</div>
 </main>
---
+++ src/LoginCard.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col bg-white p-8 rounded-xl shadow-xl">
+    <input class="px-4 h-12 my-2 border border-gray-300 rounded-md" type="text" placeholder="Email address or phone number" />
+    <input class="px-4 h-12 my-2 border border-gray-300 rounded-md" type="password" placeholder="Password" />
+    <button class="bg-blue-600 hover:bg-blue-700 text-white rounded-md py-2 my-2 font-bold">Log In</button>
+    <span class="text-blue-600 text-center text-sm my-2 cursor-pointer hover:underline">Forgotten Password?</span>
+    <hr class="my-2"/>
+    <button class="bg-green-600 hover:bg-green-700 text-white rounded-md py-2 my-2 font-bold px-4 w-fit mx-auto">Create New Account</button>
+</div>
